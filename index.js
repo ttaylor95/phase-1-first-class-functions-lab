f2bc346f44326ba9e9b23d94eb201677eed28f64
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-26 18:59:06 -0700

    commit tree 6931de6a7c25f7a35c4487fb18ea20d5915e203b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+// Declare const returnFirstTwoDrivers 
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const twoDrivers = drivers.slice(0,2);
+const lastDrivers = drivers.slice(-2);
+
+function returnFirstTwoDrivers(){
+    return twoDrivers;
+}
+
+function returnLastTwoDrivers(){
+    return lastDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//returns a function; should multiple a given value using created multiplier
+
+const createFareMultiplier = function (num) {
+    return function(fare){
+        return num * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers,func){
+    return func(drivers);
+}
